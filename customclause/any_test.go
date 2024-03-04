9c6db93ce93da789b623667046d8cc064e255cde
just-hms <alessandroversari1@gmail.com>
2024-03-04 02:36:16 +0100

    init

---
+++ customclause/any_test.go
@@ -0,0 +1,92 @@
+package customclause_test
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"testing"
+
+	"github.com/just-hms/gorm-any/customclause"
+	"github.com/stretchr/testify/require"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func TestPreloadWithAny(t *testing.T) {
+	req := require.New(t)
+
+	dsn := url.URL{
+		User:     url.UserPassword("kek", "kek"),
+		Scheme:   "postgres",
+		Host:     fmt.Sprintf("%s:%s", "host", "5432"),
+		Path:     "kek",
+		RawQuery: (&url.Values{"sslmode": []string{"disable"}}).Encode(),
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn.String()), &gorm.Config{
+		SkipDefaultTransaction: true,
+		Logger: logger.New(log.Default(), logger.Config{
+			IgnoreRecordNotFoundError: true,
+			LogLevel:                  logger.Error,
+		}),
+	})
+	req.NoError(err)
+
+	customclause.UseAny(db)
+
+	type Dog struct {
+		ID       uint `gorm:"primaryKey"`
+		PersonID uint
+	}
+
+	type Person struct {
+		ID   uint  `gorm:"primaryKey"`
+		Dogs []Dog `gorm:"foreignKey:PersonID"`
+	}
+
+	_ = db.Migrator().DropTable(&Person{}, &Dog{})
+	err = db.AutoMigrate(&Person{}, &Dog{})
+	req.NoError(err)
+
+	tx := db.Begin()
+
+	// Create a lot of data
+	const (
+		peopleCount = 70_000
+		dogsCount   = 2
+	)
+
+	people := make([]Person, 0, peopleCount)
+	for i := range peopleCount {
+		people = append(people, Person{
+			ID: uint(i) + 1,
+		})
+	}
+	err = tx.CreateInBatches(&people, 1000).Error
+	req.NoError(err)
+
+	dogs := make([]Dog, 0, dogsCount)
+	for i := range dogsCount {
+		dogs = append(dogs, Dog{PersonID: people[i].ID})
+	}
+
+	err = tx.CreateInBatches(dogs, 1000).Error
+	req.NoError(err)
+
+	people = make([]Person, 0)
+
+	// Preload and retrieve data
+	err = tx.Preload("Dogs").Find(&people).Error
+
+	req.NoError(err)
+	req.Equal(peopleCount, len(people))
+
+	for _, person := range people {
+		req.Equal(len(person.Dogs), dogsCount)
+	}
+}
+
+func BenchmarkAny(b *testing.B) {
+
+}
